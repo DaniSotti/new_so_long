fe4978a85a3a1e9fb409add17cff0dcb865e430d
Danielle de Sotti <danidesotti@gmail.com>
2023-10-09 19:48:53 +0100

    09.10.2023

--- main.c
+++ main.c
@@ -54,78 +54,19 @@ typedef struct s_data {
 	char	**temp_map;
 }	t_data;
 
-// void	draw_player(t_data *data)
-// {
-//     int		img_width;
-//     int		img_height;
-// 	void	*tardigrade;	
-// 	tardigrade = mlx_xpm_file_to_image(data->mlx, PLAYER, 
-//					&img_width, &img_height);
-//  mlx_put_image_to_window(data->mlx, data->win, tardigrade, 
-//					data->player_x *TILE_SIZE, data->player_y * TILE_SIZE);
-// }
+void	ft_clean_map(t_data *data, char **map)
+{
+	int	i;
 
- int is_collectible_blocked(t_data *data, int x, int y) {
- 	//Eu vou na posicao pra cima pra baixo e para os lados verificando se tenho 1 e retorno erro caso sim
-     return (x > 0 && data->map[y][x - 1] == '1') &&
-            (x < data->map_width - 1 && data->map[y][x + 1] == '1') &&
-            (y > 0 && data->map[y - 1][x] == '1') &&
-            (y < data->map_height - 1 && data->map[y + 1][x] == '1');
- }
-
-int is_exit_blocked(t_data *data, int x, int y) {
- 	 return (x > 0 && data->map[y][x - 1] == '1') &&
-            (x < data->map_width - 1 && data->map[y][x + 1] == '1') &&
-            (y > 0 && data->map[y - 1][x] == '1') &&
-            (y < data->map_height - 1 && data->map[y + 1][x] == '1');
- }
-
-//  int check_exit_collectibles(t_data *data) {
-//     int player_count = 0;
-//     int exit_count = 0;
-//     int collectible_count = 0;
-//     int expected_line_length = data->map_width;
-
-//     int i = 0;
-//     while (i < data->map_height) {
-//         int line_length = ft_strlen(data->map[i]);
-
-//         if (line_length != expected_line_length) {
-//             ft_printf("Line %d has an incorrect length. Expected: %d, Found: %d.\n", i + 1, expected_line_length, line_length);
-//             return 0;
-//         }
-
-//         int j = 0;
-//         while (j < data->map_width) {
-//             char cell = data->map[i][j];
-//             if (cell != 'P' && cell != '1' && cell != 'C' && cell != 'E' && cell != '0' && cell != '\n')
-//             {
-//                 ft_printf("There are different map caracteres.\n");
-//                 return 0;
-//             }
-//             if (cell == 'P') {
-//                 player_count++;
-//             } else if (cell == 'E') {
-//                 exit_count++;
-// 				if (is_exit_blocked(data, j, i)) {
-//                      ft_printf("Exit at (%d, %d) is completely blocked by walls.\n", i, j);
-//                      return 0;
-//                 }
-//             } else if (cell == 'C') {
-//                 collectible_count++;
-
-//                 // Check for collectibles blocked by walls
-//                  if (is_collectible_blocked(data, j, i)) {
-//                      ft_printf("Collectible at (%d, %d) is completely blocked by walls.\n", i, j);
-//                      return 0;
-//                  }
-//             }
-//             j++;
-//         }
-//         i++;
-//     }
-//     return 1;
-// }
+	i = 0;
+	while (i < data->map_height)
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+	// free(data->map_path);
+}
 
 void	draw_image(t_data *data, char *image_path, int x, int y)
 {
@@ -137,6 +78,7 @@ void	draw_image(t_data *data, char *image_path, int x, int y)
 			&img_width, &img_height);
 	mlx_put_image_to_window(data->mlx, data->win, image,
 		x * TILE_SIZE, y * TILE_SIZE);
+	mlx_destroy_image(data->mlx, image);
 }
 
 void	render_moves(t_data *data)
@@ -235,12 +177,21 @@ void	draw_new_exit(t_data *data)
 	}
 }
 
-void	exit_game(t_data *data)
+int exit_game(t_data *data)
 {
-	ft_printf("Ohhh! You gave up :( \n");
-	free (data->map);
-	exit (0);
+    ft_printf("Ohhh! You gave up :( \n");
+	if (data->mlx)
+	{
+		mlx_destroy_window(data->mlx, data->win);
+		mlx_destroy_display(data->mlx);
+	}
+    if (data->map_path)
+        free(data->map_path);
+    ft_clean_map(data, data->map);
+    ft_clean_map(data, data->temp_map);
+    exit(0);
 }
+
 
 void	calculate_next_position(int keycode, t_data *data)
 {
@@ -355,26 +306,28 @@ int	is_map_surrounded_by_walls(t_data *data)
 	return (1);
 }
 
-int	check_line_length(t_data *data)
-{
-	int	i;
-	int	line_length;
-	int	expected_line_length;
+// int	check_line_length(t_data *data)
+// {
+// 	int	i;
+// 	int	line_length;
+// 	int	expected_line_length;
 
-	i = 0;
-	expected_line_length = data->map_width;
-	while (i < data->map_height)
-	{
-		line_length = ft_strlen(data->map[i]);
-		if (line_length != expected_line_length)
-		{
-			ft_printf("Map Error \n");
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
+// 	i = 0;
+// 	expected_line_length = data->map_width;
+// 	while (i < data->map_height)
+// 	{
+// 		line_length = ft_strlen(data->map[i]);
+// 		if (line_length != expected_line_length)
+// 		{
+// 			ft_printf("O erro sai daqui");
+// 			ft_printf("%d\n", line_length);
+// 			ft_printf("%d\n", expected_line_length);
+// 			return (1);
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
 
 static void	check_counts(t_data *data, int height, int width)
 {
@@ -423,19 +376,7 @@ void	valid_character(t_data *data)
 	}
 }
 
-int	check_elements(t_data *data)
-{
-	check_line_length(data);
-	valid_character(data);
-	if (!is_map_surrounded_by_walls(data))
-	{
-		ft_printf("The map is not surrounded by walls.\n");
-		return (0);
-	}
-	return (1);
-}
-
-void	check_map(t_data *data)
+void	count_size_map(t_data *data)
 {
 	char	*line;
 	int		line_length;
@@ -462,33 +403,30 @@ void	check_map(t_data *data)
 	}
 }
 
-void	create_map(t_data *data)
+char	**create_map(t_data *data)
 {
+	char	**map;
 	char	*line;
 	int		i;
-	int		j;
 
-	data->map = (char **)malloc(data->map_height * sizeof(char *));
-	i = 0;
-	while (i < data->map_height)
-		data->map[i++] = (char *)malloc(data->map_width * sizeof(char));
+	map = (char **)malloc((data->map_height + 1) * sizeof(char *));
+	if (!map)
+		return (NULL);
 	data->fd = open(data->map_path, O_RDONLY);
 	i = 0;
 	line = get_next_line(data->fd);
-	while (line)
+	while (i < data->map_height && (line))
 	{
-		j = 0;
-		while (j < data->map_width)
-		{
-			data->map[i][j] = line[j];
-			j++;
-		}
-		data->map[i][j] = '\0';
+		map[i] = ft_strdup(line);
 		free(line);
+		if (!map[i])
+			return (NULL);
 		i++;
 		line = get_next_line(data->fd);
 	}
 	close(data->fd);
+	map[i] = NULL;
+	return (map);
 }
 
 void	player_initial_position(t_data *data)
@@ -514,13 +452,13 @@ void	player_initial_position(t_data *data)
 	}
 }
 
-int	handle_window_close(t_data *data)
-{
-	ft_printf("Ohhh! You gave up :(\n");
-	free(data->map);
-	exit(0);
-	return (0);
-}
+// int	handle_window_close(t_data *data)
+// {
+// 	ft_printf("Ohhh! You gave up :(\n");
+// 	free(data->map);
+// 	exit(0);
+// 	return (0);
+// }
 
 int	handle_expose(t_data *data)
 {
@@ -528,201 +466,137 @@ int	handle_expose(t_data *data)
 	return (0);
 }
 
-void	ft_clean_map(t_data *data, char **map)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->map_height)
-	{
-		free(map[i]);
-		i++;
-	}
-	free(map);
-}
-
-void clone_map(t_data *data)
-{
-   	char	*line;
-	int		i;
-	int		j;
-
-	data->temp_map = (char **)malloc(data->map_height * sizeof(char *));
-	i = 0;
-	while (i < data->map_height)
-		data->temp_map[i++] = (char *)malloc(data->map_width * sizeof(char));
-	data->fd = open(data->map_path, O_RDONLY);
-	i = 0;
-	line = get_next_line(data->fd);
-	while (line)
-	{
-		j = 0;
-		while (j < data->map_width)
-		{
-			data->temp_map[i][j] = line[j];
-			j++;
-		}
-		data->temp_map[i][j] = '\0';
-		free(line);
-		i++;
-		line = get_next_line(data->fd);
-	}
-	close(data->fd);
-}
-
-// int can_reach_point(t_data *data, int x, int y) {
-//     if (x < 0 || x >= data->map_width || y < 0 || y >= data->map_height)
-// 	{
-//          // Fora dos limites do mapa
-//          return 0;
-//  	}
-    
-//     char cell = data->temp_map[y][x];
-//     // if (cell == '1')
-// 	// {
-//     //     // A parede bloqueia o caminho
-//     //     // return 0;
-//     // } else 
-// 	if (cell == 'C')
-// 	{
-//         // Coletou um colecionável
-//         // data->temp_map[y][x] = '0';
-//         data->collectibles--;
-// 	} else if (cell == 'E')
-// 	{
-//          // Chegou à saída
-//          return 1;
-//     }
-
-//     // Tenta alcançar os pontos vizinhos
-//     int can_reach = can_reach_point(data, x + 1, y) ||
-//                     can_reach_point(data, x - 1, y) ||
-//                     can_reach_point(data, x, y + 1) ||
-//                     can_reach_point(data, x, y - 1);
-
-//     return can_reach;
-// }
-
-// // Função para verificar se o jogador pode alcançar todos os colecionáveis e a saída
-// int can_reach_collectibles_and_exit(t_data *data)
+// void	clone_map(t_data *data)
 // {
-// 	data->collectibles = data->collectible_count;
-//     int x = data->player_x;
-//     int y = data->player_y;
-//     // Verifique se o jogador pode alcançar todos os colecionáveis e a saída a partir da posição atual
-//     return can_reach_point(data, x, y) && data->collectibles == 0;
-// }
+// 	char	*line;
+// 	int		i;
+// 	int		j;
 
+// 	data->temp_map = (char **)malloc(data->map_height * sizeof(char *));
+// 	i = 0;
+// 	while (i < data->map_height)
+// 		data->temp_map[i++] = (char *)malloc(data->map_width * sizeof(char));
+// 	data->fd = open(data->map_path, O_RDONLY);
+// 	i = 0;
+// 	line = get_next_line(data->fd);
+// 	while (line)
+// 	{
+// 		j = 0;
+// 		while (j < data->map_width)
+// 		{
+// 			data->temp_map[i][j] = line[j];
+// 			j++;
+// 		}
+// 		data->temp_map[i][j] = '\0';
+// 		free(line);
+// 		i++;
+// 		line = get_next_line(data->fd);
+// 	}
+// 	close(data->fd);
+// }
 
 void	mark_path(t_data *data, int x, int y);
-void check_path(t_data *data, int x, int y);
+void	check_path(t_data *data, int x, int y);
 
-void mark_path(t_data *data, int x, int y)
+void	mark_path(t_data *data, int x, int y)
 {
-    if (x < 0 || x >= data->map_width || y < 0 || y >= data->map_height || data->temp_map[y][x] == 'V') 
-          return; // Saia se estivermos fora dos limites do mapa.
-
-    char cell = data->temp_map[y][x];
-	 
-    //  if (cell == 'P')
-    //     data->temp_map[y][x] = 'p';
-     if (cell == 'C')
-        data->collectible_count--;
-     else if (cell == 'E')
-         data->exit_count--;
+	if (x < 0 || x >= data->map_width || y < 0 || y >= data->map_height
+		|| data->temp_map[y][x] == 'V')
+		return ;
+	if (data->temp_map[y][x] == 'C')
+		data->collectible_count--;
+	else if (data->temp_map[y][x] == 'E')
+		data->exit_count--;
 	data->temp_map[y][x] = 'V';
 	check_path(data, x + 1, y);
 	check_path(data, x - 1, y);
 	check_path(data, x, y + 1);
 	check_path(data, x, y - 1);
- }
+}
 
- void check_path(t_data *data, int x, int y)
- {
-     if (x < 0 || x >= data->map_width || y < 0 || y >= data->map_height)
-         return; // Saia se estivermos fora dos limites do mapa.
+void	check_path(t_data *data, int x, int y)
+{
+	if (x < 0 || x >= data->map_width || y < 0 || y >= data->map_height)
+		return ;
+	if (data->map[y][x] == 'P' || data->map[y][x] == 'E'
+	|| data->map[y][x] == 'C' || data->map[y][x] == '0')
+		mark_path(data, x, y);
+}
 
-     char cell = data->map[y][x];
-     if (cell == 'P' || cell == 'E' || cell == 'C' || cell == '0')
-     {
-         mark_path(data, x, y);
-		 ft_printf("%c \n", cell);
-     }
- }
+int init_game(t_data *data, char *map_name)
+{
+    data->player_x = 0;
+    data->player_y = 0;
+    data->moves = 0;
+    data->prev_mov_int = 0;
+    char *s = ft_strjoin("maps/", map_name);
+	data->map_path = ft_strjoin(s, ".ber");
+	free(s);
+    if (data->map_path == NULL)
+    {
+        perror("Error creating map path");
+        return (1);
+    }
+    if (data->map_path == NULL)
+    {
+        perror("Error creating map path");
+        return (1);
+    }
+    data->fd = open(data->map_path, O_RDONLY);
+    if (data->fd == -1)
+    {
+        perror("Error opening the map file");
+		free(data->map_path);
+        return (1);
+    }
+    return (0);
+}
 
+void	check_elements(t_data *data)
+{
+	valid_character(data);
+	if (!is_map_surrounded_by_walls(data))
+	{
+		ft_printf("The map is not surrounded by walls.\n");
+		exit (1);
+	}
+	check_path(data, data->player_x, data->player_y);
+	if (data->exit_count != 0 || data->collectible_count != 0)
+	{
+		ft_printf("Player blocked.\n");
+		ft_clean_map(data, data->temp_map);
+		exit (1);
+	}
+}
 
-
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	data.player_x = 0;
-	data.player_y = 0;
-	data.moves = 0;
-	data.prev_mov_int = 0;
 	if (argc != 2)
 	{
 		ft_printf("you need to choose the map name \n");
 		exit(1);
 	}
-	data.map_path = ft_strjoin("maps/", argv[1]);
-	data.map_path = ft_strjoin(data.map_path, ".ber");
-	data.fd = open(data.map_path, O_RDONLY);
-	if (data.fd == -1)
-	{
-		perror("Error opening the map file");
-		return (1);
-	}
-	check_map(&data);
+	init_game(&data, argv[1]);
+	count_size_map(&data);
 	close(data.fd);
-	create_map(&data);
+	data.map = create_map(&data);
 	player_initial_position(&data);
-	if (!check_elements(&data))
-        return (1);
-	// if (!check_exit_collectibles(&data))
-    //     return (1);
-	  // Verifique se o jogador pode alcançar todos os colecionáveis e a saída
-	clone_map(&data);
-    // int result = can_reach_collectibles_and_exit(&data);
-
-    // if (result) {
-    //     printf("O jogador pode alcançar todos os colecionáveis e a saída.\n");
-    // } else {
-    //     printf("O jogador não pode alcançar todos os colecionáveis e/ou a saída.\n");
-    // }
-	// Chama check_path para verificar se o jogador pode alcançar todos os colecionáveis e a saída
-	 check_path(&data, data.player_x, data.player_y);
-
-	 if (data.exit_count != 0 || data.collectible_count != 0) {
-		ft_printf("%d \n",data.exit);
-		ft_printf("%d \n",data.collectibles);
-	 	ft_printf("O jogador não pode alcançar todos os colecionáveis e/ou a saída.\n");
-	 	ft_clean_map(&data, data.temp_map);
-	 	return 1;
-	 }
-	
-	// if (!can_reach_all_points(&data, data.player_x, data.player_y)) {
-    // 	ft_printf("O jogador não pode alcançar todos os colecionáveis e/ou a saída.\n");
-    // 	ft_clean_map(&data, data.temp_map);
-    // 	return 1;
-	// }	
-	// check_path(&data, data.player_x, data.player_y);
-	// if (!can_reach_collectibles_and_exit(&data, data.player_x, data.player_y))
-    // {
-    //     ft_printf("O jogador não pode alcançar todos os colecionáveis e/ou a saída.\n");
-    //     ft_clean_map(&data);
-    //     return 1;
-    // }
+	data.temp_map = create_map(&data);
+	check_elements(&data);
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, data.map_width * TILE_SIZE,
 			(data.map_height * TILE_SIZE) + BLACK_BAR_HEIGHT, "So Long");
 	render_moves(&data);
 	mlx_hook(data.win, 2, 1L << 0, handle_keypress, &data);
-	mlx_hook(data.win, 17, 1L << 0, handle_window_close, &data);
+	mlx_hook(data.win, 17, 1L << 0, exit_game, &data);
 	mlx_hook(data.win, 12, 1L << 15, handle_expose, &data);
 	mlx_loop(data.mlx);
 	draw_map(&data);
 	ft_clean_map(&data, data.map);
+	ft_clean_map(&data, data.temp_map);
+	// mlx_destroy_window(data.mlx, data.win);
+	// mlx_destroy_display(data.mlx);
 	return (0);
 }
